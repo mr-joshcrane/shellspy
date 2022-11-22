0c9a5383d4e9515e550f98cdcad22db81beccb46
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2022-11-22 12:47:18 +0000

    Adding failing test for loop

--- shellspy.go
+++ shellspy.go
@@ -1,13 +1,18 @@
 package shellspy
 
 import (
+	"io"
 	"os/exec"
 	"strings"
 )
 
-func CommandFromString(s string) (*exec.Cmd) {
+func CommandFromString(s string) *exec.Cmd {
 	commands := strings.Split(s, " ")
 	path := commands[0]
 	args := commands[1:]
 	return exec.Command(path, args...)
+}
+
+func SpySession(r io.Reader, w io.Writer) {
+
 }
--- shellspy_test.go
+++ shellspy_test.go
@@ -1,6 +1,7 @@
 package shellspy_test
 
 import (
+	"bytes"
 	"os/exec"
 	"testing"
 
@@ -26,4 +27,17 @@ func TestCommandFromString_WithNoArgsConvertsToExecutableCmd(t *testing.T) {
 	if !cmp.Equal(got, want, cmp.AllowUnexported(exec.Cmd{})) {
 		t.Fatalf(cmp.Diff(got, want, cmp.AllowUnexported(exec.Cmd{})))
 	}
+}
+
+func TestSpySession_StartsALoopThatTerminatesGivenExitFollowedByNewLine(t *testing.T) {
+	t.Parallel()
+	input := bytes.NewBufferString("echo 'one'\necho 'exit'\nexit\n")
+	buf := bytes.NewBuffer([]byte{})
+	shellspy.SpySession(input, buf)
+	got := buf.String()
+	want := "> echo 'one'\none\n\n> echo 'exit\n\nexit\n>exit\n"
+	if want != got {
+		t.Fatalf(cmp.Diff(want, got))
+	}
+
 }
