92b3252a92be0532bc361bed4db5ac474e0837cf
Josh <josh.crane512@gmail.com>
2022-12-12 21:20:43 +0800

    Adding transcript test and impl shameless green

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"os"
+
+	"github.com/mr-joshcrane/shellspy"
+)
+
 func main() {
-	
+	newFile, err := os.Create("transcript.txt")
+	if err != nil {
+		panic(err)
+	}
+	shellspy.SpySession(os.Stdin, os.Stdout, newFile)
 }
