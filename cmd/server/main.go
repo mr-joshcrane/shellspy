f73ddf1a8bc07b9a35040101569a7b416351b68d
Josh Crane <josh.crane512@gmail.com>
2023-01-27 04:48:46 +0000

    adding discard for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,7 +11,7 @@ func main() {
 	PORT := os.Getenv("PORT")
 	PASSWORD := os.Getenv("PASSWORD")
 	fmt.Println("Starting shellspy on port", PORT)
-	err := shellspy.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", PORT), shellspy.NewPassword(PASSWORD))
+	err := shellspy.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", PORT), shellspy.NewPassword(PASSWORD), os.Stdout)
 	panic(err)
 }
 
